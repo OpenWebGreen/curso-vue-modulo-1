3cfcee2b2f2db42b31d280dabd7612fdae0ac644
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2018-07-31 23:05:10 -0300

    Vídeo 01 - Configurando e executando o projeto

---
+++ main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import App from './src/App'
+
+new Vue(App).$mount('#app')
---
+++ poi.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  html: {
+    template: './public/index.html'
+  },
+  webpack: { output: { publicPath: '' } }
+}
